de7a324a0ec90ba360eae6163825c1c65a40014a
Nusky173 <simon50.lecordier@gmail.com>
2021-09-17 09:55:40 +0200

    first commit for part 2

---
+++ src/main/java/fr/istic/taa/jaxrs/dao/AgendaDao.java
@@ -0,0 +1,25 @@
+package fr.istic.taa.jaxrs.dao;
+
+import fr.istic.taa.jaxrs.entities.Meeting;
+import fr.istic.taa.jaxrs.entities.Agenda;
+
+import java.util.List;
+
+/**
+ * Dao for Agenda
+ */
+public class AgendaDao extends AbstractJpaDao<Long, Agenda> {
+
+    public AgendaDao() {
+        super(Agenda.class);
+    }
+
+    /**
+     * List Meetings of en Agenda from DB
+     * @param id of Agenda
+     * @return Meeting list
+     */
+    public List<Meeting> listMeeting(long id) {
+        return this.entityManager.createNamedQuery("listMeetingOfAgenda", Meeting.class).setParameter("id", id).getResultList();
+    }
+}
