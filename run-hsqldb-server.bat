e8a7a80a3dc29df55552b2cb00ee476d7c6f9e7a
Leloup <alexleloup@yahoo.fr>
2021-11-29 09:25:59 +0100

    Update hsql db runs

--- run-hsqldb-server.bat
+++ run-hsqldb-server.bat
@@ -1,3 +1,3 @@
 cd data
 
-java -cp ..\dependency\hsqldb-2.5.1.jar org.hsqldb.Server
+java -cp ../target/dependency/hsqldb-2.5.1.jar org.hsqldb.Server
--- show-hsqldb.bat
+++ show-hsqldb.bat
@@ -1,3 +1,1 @@
-java jar dependency\hsqldb-2.5.1.jar 
-
- 
+java -cp target/dependency/hsqldb-2.5.1.jar org.hsqldb.util.DatabaseManager
